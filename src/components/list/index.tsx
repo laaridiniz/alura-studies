cad624c46889f5f912748986e66bba80fb7f8afb
laaridiniz <diniz.laari@gmail.com>
2024-01-08 20:22:02 -0300

    percorrer array com map

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,38 +1,30 @@
 import React from 'react';
 
-// class List extends React.Component {
-//     render() {
-//         return (
-//             <ul>
-//                 <li>Item 1</li>
-//                 <li>Item 2</li>
-//                 <li>Item 3</li>
-//             </ul>
-//         );
-//     }
-// }
-
 function List() {
+
+    const tarefas = [
+        {
+            tarefa: 'React',
+            tempo: '02:00:00'
+        },{
+            tarefa: 'JavaScript',
+            tempo: '01:00:00'
+        }, {
+            tarefa: 'TypeScript',
+            tempo: '03:00:00'
+        }
+    ]
+
     return(
         <aside>
             <h2>Estudos do dia</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        JavaScript
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+              {tarefas.map((item, index) => (
+                <li key={index}>
+                    <h3>{item.tarefa}</h3>
+                    <span>{item.tempo}</span>
+                </li> 
+              ))}
             </ul>
         </aside>
     )
